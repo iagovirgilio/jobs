32350d4160d7b43fcc7ec12e61eaeb9c67c13584
Iago Virgílio <iagovirgilio@gmail.com>
2022-04-06 14:05:56 -0300

    ADD: app jobs

--- freelaway/urls.py
+++ freelaway/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('auth/', include('autenticacao.urls')),
+    path('jobs/', include('jobs.urls')),
     path('admin/', admin.site.urls),
 ]
