2129d7bd2a2874034d7791df4c70de32a830b833
Iago Virgílio <iagovirgilio@gmail.com>
2022-04-06 20:44:49 -0300

    ADD: listagem de jobs e filtro de busca

--- freelaway/urls.py
+++ freelaway/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
     path('jobs/', include('jobs.urls')),
-    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,6 @@
-from . import views
 from django.urls import path
-
-
+from . import views
 
 urlpatterns = [
-
+    path('encontrar_jobs/', views.encontrar_jobs, name="encontrar_jobs")
 ]
